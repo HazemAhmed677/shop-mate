1b953afef13d5d56fca199e08ac464386ad5a481
Hazem <hzm@gmail.com>
2024-08-21 10:11:58 +0300

    Refactor application structure and routing configuration
    - Convert `AppRouters` class to an abstract class for better structure and consistency.
    - Correctly import and use `SplashViewBody` in `SplashView` to fix the incorrect widget usage.
    - Update `main.dart` to initialize the application with `ShopMateApp` and configure routing using `AppRouters`.
    - Remove unnecessary boilerplate code from `main.dart`, simplifying the entry point of the application.

--- lib/core/utils/app_routers.dart
+++ lib/core/utils/app_routers.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:shop_mate/features/splash/presentation/views/splash_view.dart';
 
-class AppRouters {
+abstract class AppRouters {
   static const String splash = '/';
   static const String home = '/home';
   static const String login = '/login';
@@ -9,7 +9,7 @@ class AppRouters {
   static const String onBoarding = '/onBoarding';
   static const String productDetails = '/productDetails';
 
-  static GoRouter goRouter = GoRouter(routes: [
+  static final GoRouter goRouter = GoRouter(routes: [
     GoRoute(
       path: splash,
       builder: (context, state) => const SplashView(),
