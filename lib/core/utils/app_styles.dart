f14acc0277b5145a786746d9663022fe64e39856
Hazem <hzm@gmail.com>
2024-08-21 10:21:11 +0300

    Add Inter font and responsive text utility in app styles
    Introduce the Inter font family with a semi-bold style for enhanced typography in the application. This change includes the addition of the Inter_18pt-SemiBold.ttf font file and its registration in the pubspec.yaml file.
    
    Furthermore, a new utility class `AppStyles` has been implemented in the `app_styles.dart` file within the `lib/core/utils` directory. This class provides a method to define text styles responsively based on the screen width, ensuring consistent and readable text across various device sizes. The `semiBold58` text style is an example of how to use the Inter font within the application. Additionally, the `getResponsiveText` method calculates a responsive font size based on a base size and screen dimensions, with clamping to maintain readability. The `getScaleFactor` method is used internally to determine the scaling factor relative to a standard screen width of 400 units.

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static TextStyle semiBold58 = const TextStyle(
+    fontSize: 58,
+    fontFamily: 'Inter',
+  );
+
+  double getResponsiveText(
+      {required BuildContext context, required double base}) {
+    double scaleFactor = getScaleFactor(
+      context: context,
+    );
+    double responsiveText = scaleFactor * base;
+    double lowerLimit = responsiveText * 0.8;
+    double upperLimit = responsiveText * 1.2;
+    return responsiveText.clamp(lowerLimit, upperLimit);
+  }
+
+  double getScaleFactor({required BuildContext context}) {
+    double widthOfScreen = MediaQuery.of(context).size.width;
+    return widthOfScreen / 400;
+  }
+}
