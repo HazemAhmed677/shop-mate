c61eb284754da9aac96ec6c4dc212c7a4f47f15e
Hazem <hzm@gmail.com>
2024-08-21 10:25:00 +0300

    Add AppColors constants to core utils
    This commit introduces a new file, `app_colors.dart`, within the `lib/core/utils` directory. The file defines an abstract class `AppColors` that encapsulates color constants used throughout the application. These constants include `white`, `primaryColor`, `n28`, `n24`, and `n48`, providing a centralized location for color management and ensuring consistency in the app's design.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color white = Colors.white;
+  static const Color primaryColor = Color(0xff704F38);
+  static const Color n28 = Color(0xff282932);
+  static const Color n24 = Color(0xff242424);
+  static const Color n48 = Color(0xff484848);
+}
