895a475162c7e46077d76d33f67088d571d65d63
Hazem <hzm@gmail.com>
2024-08-21 15:44:44 +0300

    Refactor onboarding screens to include dynamic content and styling
    Updated the SecondOnboarding and ThirdOnboarding widgets to utilize the OnboardingHeader widget for consistent layout and styling. Integrated AppImages, AppColors, and AppStyles for dynamic image loading, color application, and text styling respectively. This change enhances the visual consistency and user experience across the onboarding process by ensuring all screens follow a unified design language.

--- lib/features/onboarding/presentation/views/widgets/second_onboarding.dart
+++ lib/features/onboarding/presentation/views/widgets/second_onboarding.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:shop_mate/core/utils/app_images.dart';
+import 'package:shop_mate/features/onboarding/presentation/views/widgets/onboarding_header.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_styles.dart';
 
 class SecondOnboarding extends StatelessWidget {
   const SecondOnboarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return OnboardingHeader(
+      image: AppImages.onboarding2,
+      title: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Whishlist: ',
+                style: AppStyles.semiBoldPoppins28,
+              ),
+              Text(
+                'Where ',
+                style: AppStyles.semiBoldPoppins28,
+              ),
+              Text(
+                'Fashion',
+                style: AppStyles.semiBoldPoppins28.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Dreams ',
+                style: AppStyles.semiBoldPoppins28,
+              ),
+              Text(
+                'Begin',
+                style: AppStyles.semiBoldPoppins28.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/features/onboarding/presentation/views/widgets/third_onboarding.dart
+++ lib/features/onboarding/presentation/views/widgets/third_onboarding.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shop_mate/core/utils/app_images.dart';
+import 'package:shop_mate/features/onboarding/presentation/views/widgets/onboarding_header.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_styles.dart';
 
 class ThirdOnboarding extends StatelessWidget {
   const ThirdOnboarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return OnboardingHeader(
+      image: AppImages.onboarding3,
+      title: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Swift ',
+                style: AppStyles.semiBoldPoppins28.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+              Text(
+                'and ',
+                style: AppStyles.semiBoldPoppins28,
+              ),
+              Text(
+                'Reliable',
+                style: AppStyles.semiBoldPoppins28.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            'Delivery',
+            style: AppStyles.semiBoldPoppins28,
+          ),
+        ],
+      ),
+    );
   }
 }
