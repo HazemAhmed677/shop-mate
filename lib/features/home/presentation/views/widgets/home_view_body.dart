981f98c22bcbcba5772ef93adb1f99581f39eb35
Hazem <hzm@gmail.com>
2024-08-22 09:47:19 +0300

    Add notification icon and update imports in HomeViewBody widget
    - Import FontAwesomeIcons and AppColors for use in the widget.
    - Add a CircleAvatar with a notification bell icon to the right side of the CustomLocation widget in the HomeViewBody.
    - Update padding declaration to be a const.
    - Adjust SliverToBoxAdapter to be const where applicable.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shop_mate/core/utils/app_colors.dart';
 import 'custom_location.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -6,8 +8,8 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(
+    return Padding(
+      padding: const EdgeInsets.symmetric(
         horizontal: 22.0,
       ),
       child: SafeArea(
@@ -16,14 +18,25 @@ class HomeViewBody extends StatelessWidget {
         right: false,
         child: CustomScrollView(
           slivers: [
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SizedBox(
                 height: 32,
               ),
             ),
             SliverToBoxAdapter(
               child: Row(
-                children: [CustomLocation()],
+                children: [
+                  const CustomLocation(),
+                  const Spacer(),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: AppColors.primaryColor.withOpacity(0.2),
+                    child: const Icon(
+                      FontAwesomeIcons.solidBell,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
               ),
             ),
           ],
