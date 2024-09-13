f2bc41a4c23703006b461a9d0c24c60fbe0fe52c
Hazem <hzm@gmail.com>
2024-09-13 23:10:49 +0300

    Refactor CustomProductDetailsColumn widget to improve readability
    Extract the top section of the `CustomProductDetailsColumn` widget into a separate `ProductDetailsColumnTopSection` widget. This refactor improves code readability and maintainability by encapsulating related UI components into their own dedicated widget. The new `ProductDetailsColumnTopSection` widget includes the product style, name, details, and rating, which were previously part of the main column widget. This change also adheres to the Single Responsibility Principle, making future modifications and testing easier.

---
+++ lib/features/home/presentation/views/widgets/product_details_column_top_section.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:shop_mate/features/home/presentation/views/widgets/custom_rate_widget.dart';
+
+import '../../../../../core/utils/app_styles.dart';
+
+class ProductDetailsColumnTopSection extends StatelessWidget {
+  const ProductDetailsColumnTopSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              r"Female's Style",
+              style: AppStyles.regular24.copyWith(
+                color: Colors.grey,
+                fontSize: 18,
+              ),
+            ),
+            const Spacer(),
+            const CustomRateWidget(
+              width: 6,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          r"Light Brown Jacket",
+          style: AppStyles.regular24.copyWith(
+            fontSize: 22,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Product Details",
+          style: AppStyles.semiBoldInter16.copyWith(
+            fontSize: 20,
+          ),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        Text(
+          'kjbjkbbibobb iuhon ibon ooiij pijo oo ono oni [j op ojo po uh oih  bo hohoihhpjio joi po joip0 oh 0 9 opjpok po[ [ k[[ [ [k [kopjpj p joij oi pijpj p jpj pj p j]]]]]]]',
+          style: AppStyles.semiBoldInter16.copyWith(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+}
