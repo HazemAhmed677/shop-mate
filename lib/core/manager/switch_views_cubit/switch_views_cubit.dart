7d289c52dd020d4e19d02118bcfc3821bda469b7
Hazem <hzm@gmail.com>
2024-09-14 04:05:16 +0300

    Add SwitchViewsCubit for managing view states
    Created a new `SwitchViewsCubit` class in `lib/core/manager/switch_views_cubit/switch_views_cubit.dart` to manage the state of different views in the application. The cubit allows switching between Home, Search, and Saved views based on the provided index.
    
    Also added the corresponding `SwitchViewsState` class in `lib/core/manager/switch_views_cubit/switch_views_state.dart` to define the various states that the cubit can emit.
    
    Updated `pubspec.yaml` to include the `bloc` package dependency and updated `pubspec.lock` to reflect the new dependency version.

---
+++ lib/core/manager/switch_views_cubit/switch_views_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+
+part 'switch_views_state.dart';
+
+class SwitchViewsCubit extends Cubit<SwitchViewsState> {
+  SwitchViewsCubit() : super(HomeViewState());
+  int index = 0;
+  emitViews({required int currentIndex}) {
+    if (currentIndex == 0) {
+      index = 0;
+      emit(HomeViewState());
+    } else if (currentIndex == 1) {
+      index = 1;
+      emit(CartViewState());
+    } else if (currentIndex == 2) {
+      index = 2;
+      emit(FavoriteViewState());
+    } else if (currentIndex == 3) {
+      index = 3;
+      emit(SearchViewState());
+    } else {
+      index = 4;
+      emit(ProfileViewState());
+    }
+  }
+}
---
+++ lib/core/manager/switch_views_cubit/switch_views_state.dart
@@ -0,0 +1,13 @@
+part of 'switch_views_cubit.dart';
+
+sealed class SwitchViewsState {}
+
+final class HomeViewState extends SwitchViewsState {}
+
+final class CartViewState extends SwitchViewsState {}
+
+final class FavoriteViewState extends SwitchViewsState {}
+
+final class SearchViewState extends SwitchViewsState {}
+
+final class ProfileViewState extends SwitchViewsState {}
