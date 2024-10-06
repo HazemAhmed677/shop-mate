fb092f4a22a99adc50b4e267a6f6fdc781e34f78
Hazem <hzm@gmail.com>
2024-10-06 18:18:40 +0300

    Add testEasyCode constant to constants.dart
    This commit introduces a new constant `testEasyCode` to the `constants.dart` file. The constant is currently empty and is intended for future use in testing or other functionalities within the project. This addition ensures that the project is prepared for upcoming changes that may require this constant.

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,4 @@ const String kMyToken =
     'sk_test_51PmiHeRs5W8L4MjRAenJvaJz67KWoyro0jh3oKofDAjpDQPYfQeFkeHFbTcNj61S15Mi22GP3UF5ZqNrHLeDyL3s00qU81CkXP';
 const String kPublishableKey =
     'pk_test_51PmiHeRs5W8L4MjRGqv9cQdPYpLd0To7myrk4jr3I23Pfjjf56M3kINCe3UnkWcisEvpzXMgGaIwjltA6hrulnmD00UKntNrZi';
+const testEasyCode = '';
